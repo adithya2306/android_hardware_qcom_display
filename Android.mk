b612c47a0069a26346d8df32b3d6b3e7b8b63d6d
Ethan Chen <intervigil@gmail.com>
2014-11-08 23:27:23 -0800

    display: Use project pathmap
    Change-Id: Iabcd263ecf9f4eb28f4cd53862ba663c81275c10

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-display))
+
 ifneq ($(TARGET_DISABLE_DISPLAY),true)
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug
@@ -23,4 +25,6 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
 endif
